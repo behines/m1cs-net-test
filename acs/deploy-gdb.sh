f79b897a2c4d78bb001be4786707941adc26c354
Brad Hines <Brad.Hines@PlanetA.Energy>
2022-09-06 22:35:51 -0700

    Working

--- acs/deploy-gdb.sh
+++ acs/deploy-gdb.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 readonly TARGET_IP="$1"
 readonly PROGRAM="$2"
-readonly TARGET_DIR="$3" #"/home/root/bin"
-readonly TARGET_USERNAME="$4"
-readonly LOCAL_BIN_DIR="$5" # "../bin"
+readonly PROGRAM_ARGS="$3"
+readonly TARGET_DIR="$4" #"/home/root/bin"
+readonly TARGET_USERNAME="$5"
+readonly LOCAL_BIN_DIR="$6" # "../bin"
 
 # Must match startsPattern in tasks.json
 echo "Deploying to target ${TARGET_IP}"
@@ -21,4 +22,4 @@ scp ${LOCAL_BIN_DIR}/${PROGRAM} ${TARGET_USERNAME}@${TARGET_IP}:${TARGET_DIR}
 echo "Starting GDB Server on Target"
 
 # start gdbserver on target
-ssh -t ${TARGET_USERNAME}@${TARGET_IP} "sh -c 'cd ${TARGET_DIR}; gdbserver localhost:3000 ${PROGRAM}'"
+ssh -t ${TARGET_USERNAME}@${TARGET_IP} "sh -c 'cd ${TARGET_DIR}; gdbserver localhost:3000 ${PROGRAM} ${PROGRAM_ARGS}'"
