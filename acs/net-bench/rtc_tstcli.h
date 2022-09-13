94b83f445467e561269ea1f7ff649218f02308e1
Brad Hines <Brad.Hines@PlanetA.Energy>
2022-09-12 17:03:26 -0700

    cpp files work, whole tree compiles

---
+++ acs/net-bench/rtc_tstcli.h
@@ -0,0 +1,12 @@
+/****************************************************
+* rtc_tstcli
+*
+* Main program for GLC-LSEB benchmark testing.
+*/
+
+#ifndef INC_rtc_tstcli_h
+#define INC_rtc_tstcli_h
+
+int process_tlm(int sockfd);
+
+#endif  // INC_rtc_tstcli_h
